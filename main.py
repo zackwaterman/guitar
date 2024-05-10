56ee87abf45bb2c38c44664596d5bd2425eaab5b
Zack Waterman <zack.waterman@watermancs.com>
2024-05-10 14:05:32 -0700

    stopwatch

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import time
+import csv
+import random
+
+def generate_random_key():
+    keys = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B']
+    return random.choice(keys)
+
+class Stopwatch:
+    def __init__(self):
+        self.start_time = None
+        self.end_time = None
+
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self):
+        self.end_time = time.time()
+
+    def elapsed_time(self):
+        if self.start_time is None:
+            return 0
+        elif self.end_time is None:
+            return time.time() - self.start_time
+        else:
+            return self.end_time - self.start_time
 
 
